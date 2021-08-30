59e4a084084d330c16a47981f34be198bdaea94c
Your Name <mohitkumar591998@gmail.com>
2021-08-30 22:06:00 +0530

    day1

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-          child: Center(
-        child: Container(
-          child: Text('Welcome to flutter'),
-        ),
-      )),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          primarySwatch: Colors.orange,
+          fontFamily: GoogleFonts.lato().fontFamily),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      //initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
